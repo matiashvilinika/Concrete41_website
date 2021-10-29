ebfae2207752c97af650c0284c30cb34211b1b2c
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2021-10-29 23:27:50 +0400

    first commit

---
+++ scripts/ashtray.js
@@ -0,0 +1,15 @@
+const apiResult = [{
+    title:"qotanski",
+    Color1: "#111",
+    Color2: "yellow",
+    Color3: "",
+    Color4: "",
+    Color5: "",
+    Color6: "",
+    Color7: "",
+    Price: "12",
+    Code: "pot1",
+    Size1: "20",
+    Size2: "30",
+    Size3: "33"
+  },];
